3f2523f663ddc3b67240ec122d66ece91875fd10
RehamTammam1 <rtammam0@gmail.com>
2024-06-20 05:54:35 +0300

    Adding new routes to about , contact , meet the team pages

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/meet-the-team', function () {
+    return view('meetTheTeam');
 });
