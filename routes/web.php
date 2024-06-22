2ba632698f98e2ff6a26bafb5efd13da851ea427
RehamTammam1 <rtammam0@gmail.com>
2024-06-23 02:05:54 +0300

    Creating a jobs and job route to render jobs

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,63 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Arr;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/jobs', function () {
+    return view('jobs',[
+        'jobs' => [
+            [   
+                'id' => 1,
+                'Title'=>'Director',
+                'Salary'=>'40K'
+
+            ],
+            [   
+                'id'=> 2,
+                'Title'=>'DEVELOPER',
+                'Salary'=>'60K'
+
+            ],
+            [   
+                'id' => 3,
+                'Title'=>'Graphic Designer',
+                'Salary'=>'70K'
+
+            ]
+        ]
+     
+    ]);
+});
 Route::get('/about', function () {
     return view('about');
+});
+Route::get('/jobs/{id}', function ($id) {
+   
+   $jobs = [  
+        [   
+        'id' => 1,
+        'Title'=>'Director',
+        'Salary'=>'40K'
+        ],
+        [   
+        'id'=> 2,
+        'Title'=>'DEVELOPER',
+        'Salary'=>'60K'
+
+        ],
+        [   
+        'id' => 3,
+        'Title'=>'Graphic Designer',
+        'Salary'=>'70K'
+
+        ]
+    ];
+ 
+    $job = Arr::first($jobs,fn($job)=>$job['id']==$id);
+ 
+    return view('job',['job'=>$job]);
 });
 Route::get('/contact', function () {
     return view('contact');
