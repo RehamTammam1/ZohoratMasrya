d03f86f8abe08411752b885aa50da0e03648b0b4
RehamTammam1 <rtammam0@gmail.com>
2024-06-23 02:05:27 +0300

    Creating a view for a job details

---
+++ resources/views/job.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+     <x-slot:heading>Job </x-slot>
+    
+        <ul>
+            
+                <li>
+                    <strong>{{$job['Title']}}:</strong> Pays {{$job['Salary']}}
+                </li
+        
+        </ul>
+
+     
+
+     </x-layout>
