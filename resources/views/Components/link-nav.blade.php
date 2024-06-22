bcd81907b14a946e3f1e52905ca5ee9748e83b52
RehamTammam1 <rtammam0@gmail.com>
2024-06-22 19:59:38 +0300

    Creating a link and button compments

--- resources/views/Components/link-nav.blade.php
+++ resources/views/Components/link-nav.blade.php
@@ -1,2 +1,12 @@
- <a href="/{{$slot}}" >{{$slot}}</a>
+@props(['active'=>False,'type'=>['a','button']])
 
+@if($type == 'a') 
+
+ <a class="{{ $active ?'bg-rose-400 text-white':'text-gray-300 hover:bg-gray-700 hover:text-white'}}"
+  aria-current="page"{{$attributes}}>{{$slot}}</a>
+
+@else
+    <button class="{{ $active ?'bg-rose-400 text-white':'text-gray-300 hover:bg-gray-700 hover:text-white'}}"
+  aria-current="page"{{$attributes}}>{{$slot}}</button>
+
+@endif
