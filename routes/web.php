1238cbc178aba5e53bf70e7f611f78dba51f1cc3
RehamTammam1 <rtammam0@gmail.com>
2024-06-23 21:14:25 +0300

    Refacotring routes

--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Arr;
+use App\Models\job;
 
-$jobs = [  
-    [   
-    'id' => 1,
-    'Title'=>'Director',
-    'Salary'=>'40K'
-    ],
-    [   
-    'id'=> 2,
-    'Title'=>'DEVELOPER',
-    'Salary'=>'60K'
-
-    ],
-    [   
-    'id' => 3,
-    'Title'=>'Graphic Designer',
-    'Salary'=>'70K'
-
-    ]
-];
 
 
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/jobs', function () use ($jobs) {
-    return view('jobs',[ 'jobs' =>  $jobs ]);
+Route::get('/jobs', function ()  {
+    return view('jobs',[ 'jobs' =>  job::all() ]);
 });
 
 Route::get('/about', function () {
     return view('about');
 });
-Route::get('/jobs/{id}', function ($id) use($jobs) {
+Route::get('/jobs/{id}', function ($id) {
    
- 
- 
-    $job = Arr::first($jobs,fn($job)=>$job['id']==$id);
+
+    $job = job::find($id);
  
     return view('job',['job'=>$job]);
 });
